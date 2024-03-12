9ebf9cc384147b0486594a392397c112893f4bd9
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-03-13 00:55:57 +0100

    3 alphabet game again

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for l in range(97,123,1):
-    if (l != 101 or l != 113):
+    if (l == 101 or l == 113):
+        continue
+    else:
         print("{}".format(chr(l)), end='')
