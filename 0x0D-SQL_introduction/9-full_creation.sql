e4c8cf0c64cbd260259a3bc8cfe2ca3bab8d59f1
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-05-23 23:16:19 +0100

    create second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,9 +6,7 @@ CREATE TABLE IF EXISTS description(
 
 );
 
-INSERT INTO description (id, name, score)
-VALUES
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 10);
+INSERT INTO description(id, name, score) VALUES (1, "John", 10);
+INSERT INTO description(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO description(id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO description(id, name, score) VALUES (4, "George", 10);
