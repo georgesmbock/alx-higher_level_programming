4e962798a2948356738b34783b66c9fa34c2fdbe
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-06-19 02:20:33 +0100

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(0);
+const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
-  console.log('Argumment found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
