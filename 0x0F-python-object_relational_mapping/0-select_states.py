15810bc709a84687a7770f8616cf4f0afcfc97a7
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-06-23 19:07:21 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 # Mapping a python script to MySQL database
-import sys
-import MySQLdb
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        port=3306,
-        password=sys.argv[2],
-        db=sys.argv[3]
-        )
-c = db.cursor()
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            port=3306,
+            password=sys.argv[2],
+            db=sys.argv[3]
+            )
+    c = db.cursor()
 
-c.execute("SELECT * FROM states ORDER BY id ASC")
+    c.execute("SELECT * FROM states ORDER BY id ASC")
 
-states = c.fetchall()
-for state in states:
-    print(state)
-c.close()
-db.close()
+    states = c.fetchall()
+    for state in states:
+        print(state)
+    c.close()
+    db.close()
